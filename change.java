a993762f777e544fb5401214bdaaad9596eb0664
Mitsuka Kiyohara <mitsukakiyohara@Mitsukas-MBP.attlocal.net>
2020-09-02 13:23:19 -0700

    initial hello world

---
+++ change.java
@@ -0,0 +1,12 @@
+public class change
+{
+    //run program by java program.java on terminal
+    //when running specific method in java, first run javac change.java, then run java methodname
+    public static void main(String[] args)
+    {
+      System.out.println("hello world");
+    }
+    //to see contents of java program on terminal, type cat
+
+
+}
