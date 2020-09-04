688237c606c0dd2241c5487ea69a133e17963215
Mitsuka Kiyohara <mitsukakiyohara@Mitsukas-MBP.attlocal.net>
2020-09-04 13:07:31 -0700

    update on change file

--- change.java
+++ change.java
@@ -1,12 +1,33 @@
 public class change
 {
+    public static int[] coins = {5, 10, 25};
     //run program by java program.java on terminal
     //when running specific method in java, first run javac change.java, then run java methodname
     public static void main(String[] args)
     {
-      System.out.println("hello world");
+      int cents = 11;
+      System.out.println(NumberofWaystoMake(cents, 0)); //second parameter, currentCoin is to help remove duplicates
     }
-    //to see contents of java program on terminal, type cat
 
+    public static int NumberofWaystoMake(int cents, int currentCoin)
+    {
+      //using recursion
+      if (cents < 5)
+      {
+        return 1;
+      }
+      else if (cents < 0)
+      {
+        return 0;
+      }
+      int numWays = 0;
+      for (int coin = currentCoin; coin < coins.length; coin++)
+      {
+          numWays += NumberofWaystoMake(cents - coins[coin], coin);
+      }
+
+      return numWays - 1; //always count 1 more... using magic number to solve this issue (committed a crime)
+
+    }
 
 }
